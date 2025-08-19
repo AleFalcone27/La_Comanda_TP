6139ee90e9b64291cb9d27ef0e2d35c3b4b9e1e3
Alejo Falcone <alejo.falcone@sovanta.com>
2025-08-19 20:07:01 -0300

    replace strings for Roles enum values

--- app/index.php
+++ app/index.php
@@ -31,46 +31,46 @@ $app->group('/usuario', function (RouteCollectorProxy $group) {
     $group->get('/operaciones', \UsuarioControles::class . ':obtenerOperaciones');
     $group->get('/operacionesSector', \UsuarioControles::class . ':obtenerOperacionesPorSector');
     $group->delete('/borrar/{id_usuario}', \UsuarioControles::class . ':BorrarUno');
-  })->add(new AccesoMiddleware(["socio"]));
+  })->add(new AccesoMiddleware([ Roles::SOCIO->value ]));
 
 
 // Handlers Producto
 $app->group('/producto', function (RouteCollectorProxy $group) {
   $group->get('[/]', \ProductoControles::class . ':TraerTodos');
-  $group->post('[/]', \ProductoControles::class . ':CargarUno')->add(new AccesoMiddleware(["socio", "cocinero"]));
-  $group->get('/menosVendido', \ProductoControles::class . ':TraerOrdenados')->add(new AccesoMiddleware(["socio"]));
-  $group->put('/modificar', \ProductoControles::class . ':ModificarUno')->add(new AccesoMiddleware(["socio"]));
-  $group->delete('/borrar/{id_producto}', \ProductoControles::class . ':BorrarUno')->add(new AccesoMiddleware(["socio"]));
+  $group->post('[/]', \ProductoControles::class . ':CargarUno')->add(new AccesoMiddleware([Roles::SOCIO->value, Roles::COCINERO->value]));
+  $group->get('/menosVendido', \ProductoControles::class . ':TraerOrdenados')->add(new AccesoMiddleware([Roles::SOCIO->value,]));
+  $group->put('/modificar', \ProductoControles::class . ':ModificarUno')->add(new AccesoMiddleware([Roles::SOCIO->value,]));
+  $group->delete('/borrar/{id_producto}', \ProductoControles::class . ':BorrarUno')->add(new AccesoMiddleware([Roles::SOCIO->value,]));
 });
 
 // Handlers Orden
 $app->group('/orden', function (RouteCollectorProxy $group) {
-  $group->get('[/]', \OrdenControles::class . ':TraerTodos')->add(new AccesoMiddleware(["socio", "mozo"]));
-  $group->post('[/]', \OrdenControles::class . ':CargarUno')->add(new AccesoMiddleware(["mozo"]));
-  $group->post('/imagen', \OrdenControles::class . ':AgregarImagen')->add(new AccesoMiddleware(["mozo"]));
+  $group->get('[/]', \OrdenControles::class . ':TraerTodos')->add(new AccesoMiddleware([Roles::SOCIO->value, Roles::MOZO->value]));
+  $group->post('[/]', \OrdenControles::class . ':CargarUno')->add(new AccesoMiddleware([Roles::MOZO->value]));
+  $group->post('/imagen', \OrdenControles::class . ':AgregarImagen')->add(new AccesoMiddleware([Roles::MOZO->value]));
   $group->get('/demora', \OrdenControles::class . ':DemoraOrden');
-  $group->get('/lista', \OrdenControles::class . ':ListaOrdenes')->add(new AccesoMiddleware(["socio", "mozo","bartender","cocinero"]));
-  $group->put('/servir', \OrdenControles::class . ':ServirOrden')->add(new AccesoMiddleware(["mozo"]));
-  $group->put('/cobrar', \OrdenControles::class . ':CobrarOrden')->add(new AccesoMiddleware(["mozo"]));
-  $group->get('/tarde', \OrdenControles::class . ':OrdenesEntregadasTarde')->add(new AccesoMiddleware(["socio"]));
-  $group->get('/aTiempo', \OrdenControles::class . ':OrdenesEntregadasATiempo')->add(new AccesoMiddleware(["socio"]));
+  $group->get('/lista', \OrdenControles::class . ':ListaOrdenes')->add(new AccesoMiddleware([Roles::SOCIO->value, Roles::MOZO->value,Roles::BARTENDER->value, Roles::COCINERO->value]));
+  $group->put('/servir', \OrdenControles::class . ':ServirOrden')->add(new AccesoMiddleware([Roles::MOZO->value]));
+  $group->put('/cobrar', \OrdenControles::class . ':CobrarOrden')->add(new AccesoMiddleware([Roles::MOZO->value]));
+  $group->get('/tarde', \OrdenControles::class . ':OrdenesEntregadasTarde')->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->get('/aTiempo', \OrdenControles::class . ':OrdenesEntregadasATiempo')->add(new AccesoMiddleware([Roles::SOCIO->value]));
 });
 
 // Handlers Ventas
 $app->group('/ventas', function (RouteCollectorProxy $group) {
-  $group->get('[/]', \VentaControles::class . ':TraerTodos')->add(new AccesoMiddleware(["socio"]));
+  $group->get('[/]', \VentaControles::class . ':TraerTodos')->add(new AccesoMiddleware([Roles::SOCIO->value]));
   $group->get('/rol', \VentaControles::class . ':TraerVentasRol')->add(new RolMiddleware());
 });
 
 // Handlers Mesa
 $app->group('/mesa', function(RouteCollectorProxy $group){
-  $group->post('[/]', \MesaControles::class . ':CargarUno')->add(new AccesoMiddleware(["socio"]));
+  $group->post('[/]', \MesaControles::class . ':CargarUno')->add(new AccesoMiddleware([Roles::SOCIO->value]));
   $group->put('/modificar', \MesaControles::class . ':ModificarUno')->add(new RolMiddleware());
-  $group->get('[/]', \MesaControles::class . ':TraerTodos')->add(new AccesoMiddleware(["socio", "mozo"]));
-  $group->get('/masUsada', \MesaControles::class . ':MasUsada')->add(new AccesoMiddleware(["socio"]));
-  $group->get('/facturacion', \MesaControles::class . ':MenosFacturo')->add(new AccesoMiddleware(["socio"]));
-  $group->get('/facturacionFechas', \MesaControles::class . ':FacturoFechas')->add(new AccesoMiddleware(["socio"]));
-  $group->delete('/borrar/{id_mesa}', \MesaControles::class . ':BorrarUno')->add(new AccesoMiddleware(["socio"]));
+  $group->get('[/]', \MesaControles::class . ':TraerTodos')->add(new AccesoMiddleware([Roles::SOCIO->value, Roles::MOZO->value]));
+  $group->get('/masUsada', \MesaControles::class . ':MasUsada')->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->get('/facturacion', \MesaControles::class . ':MenosFacturo')->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->get('/facturacionFechas', \MesaControles::class . ':FacturoFechas')->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->delete('/borrar/{id_mesa}', \MesaControles::class . ':BorrarUno')->add(new AccesoMiddleware([Roles::SOCIO->value]));
 });
 
 $app->group('/preparar', function (RouteCollectorProxy $group) {
@@ -79,14 +79,14 @@ $app->group('/preparar', function (RouteCollectorProxy $group) {
 });
 
 $app->group('/descargar', function (RouteCollectorProxy $group) {
-  $group->get('/csv', function ($request, $response, $args){ return descargarCSV($request, $response, $args);})->add(new AccesoMiddleware(["socio"]));
-  $group->post('/csv', function ($request, $response, $args){ return cargarCSV($request, $response, $args);})->add(new AccesoMiddleware(["socio"]));
-  $group->get('/logoPDF', function ($request, $response, $args){ return LogoPDfDescarga($request, $response, $args);})->add(new AccesoMiddleware(["socio"]));
+  $group->get('/csv', function ($request, $response, $args){ return descargarCSV($request, $response, $args);})->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->post('/csv', function ($request, $response, $args){ return cargarCSV($request, $response, $args);})->add(new AccesoMiddleware([Roles::SOCIO->value]));
+  $group->get('/logoPDF', function ($request, $response, $args){ return LogoPDfDescarga($request, $response, $args);})->add(new AccesoMiddleware([Roles::SOCIO->value]));
 });
 
 $app->group('/encuesta', function (RouteCollectorProxy $group) {
   $group->post('[/]', \EncuestaControles::class . ':CargarUno');
-  $group->get('/mejores', \EncuestaControles::class . ':MejoresEncuestas')->add(new AccesoMiddleware(["socio"]));
+  $group->get('/mejores', \EncuestaControles::class . ':MejoresEncuestas')->add(new AccesoMiddleware([Roles::SOCIO->value]));
 });
 
 $app->run();
