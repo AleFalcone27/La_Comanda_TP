d56f8ed71b7869b4f020c48f50c80822e88d73d4
Alejo Falcone <alejo.falcone@sovanta.com>
2025-08-19 19:58:21 -0300

    add Roles enum

--- app/middlewares/MwRol.php
+++ app/middlewares/MwRol.php
@@ -5,6 +5,15 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Psr\Http\Server\MiddlewareInterface as MiddlewareInterface;
 use Slim\Psr7\Factory\ResponseFactory;
 
+
+enum Roles: string {
+    case SOCIO = "socio";
+    case BARTENDER = "bartender";
+    case MOZO = "mozo";
+    case COCINERO = "cocinero";
+    case CERVERCERO = "cervecero";
+}
+
 class RolMiddleware{
     public function __invoke(Request $request, RequestHandler $handler): Response {
         $responseFactory = new ResponseFactory();
