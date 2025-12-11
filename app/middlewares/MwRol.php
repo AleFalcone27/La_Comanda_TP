518295a1bb6021d44982386605b35d36a54e1da4
Alejo Falcone <alejo.falcone@sovanta.com>
2025-12-11 13:03:28 -0300

    refactor remove unnecessary validations

--- app/middlewares/MwRol.php
+++ app/middlewares/MwRol.php
@@ -1,11 +1,13 @@
 <?php
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Slim\Psr7\Factory\ResponseFactory;
 
-enum Roles: string {
+enum Roles: string
+{
     case SOCIO = "socio";
     case BARTENDER = "bartender";
     case MOZO = "mozo";
@@ -13,38 +15,28 @@ enum Roles: string {
     case CERVERCERO = "cervecero";
 }
 
-class RolMiddleware{
+class RolMiddleware
+{
 
     private $rolAcceso;
 
-    public function __construct($rolAcceso) {
+    public function __construct($rolAcceso)
+    {
         $this->rolAcceso = $rolAcceso;
     }
 
-    public function __invoke(Request $request, RequestHandler $handler): Response {
-     
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+
         $responseFactory = new ResponseFactory();
 
-        $header = $request->getHeaderLine('Authorization');
-
-        $token = trim(explode("Bearer", $header)[1]);
- 
-        try {     
-            $data = AutentificadorJWT::ObtenerData($token);
-            
-            if (in_array($data->rol, $this->rolAcceso)) {
-                return $handler->handle($request);
-            } else {
-                $response = $responseFactory->createResponse();
-                $response = $response->withHeader('Content-Type', 'application/json');
-                $response->getBody()->write(json_encode(['Error' => 'Acceso denegado']));
-                return $response->withStatus(403);
-            }
-        } catch (Exception $e) {
+        if (in_array($request->getAttribute("rol"), $this->rolAcceso)) {
+            return $handler->handle($request);
+        } else {
             $response = $responseFactory->createResponse();
             $response = $response->withHeader('Content-Type', 'application/json');
-            $response->getBody()->write(json_encode(['Error' => 'Token inválido', 'message' => $e->getMessage()]));
-            return $response->withStatus(401);
+            $response->getBody()->write(json_encode(['Error' => 'Acceso denegado']));
+            return $response->withStatus(403);
         }
     }
 }
